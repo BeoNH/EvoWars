5e88712aefe838bc832ae4b54770a191ff94b600
Nguyễn Huy Hoàng <hoangnguyenhbs@gmail.com>
2024-06-27 17:34:29 +0700

    up

--- assets/Script/Bot.ts
+++ assets/Script/Bot.ts
@@ -1,6 +1,5 @@
-import { _decorator, Animation, BoxCollider, BoxCollider2D, CircleCollider2D, Collider2D, Component, Contact2DType, IPhysics2DContact, misc, Node, Sprite, Tween, tween, UIOpacity, Vec3 } from 'cc';
+import { _decorator, Animation, BoxCollider2D, CircleCollider2D, Collider2D, Component, Contact2DType, IPhysics2DContact, misc, Node, Sprite, Tween, tween, UIOpacity, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
-
 
 export enum BOT_Type {
     STOP,
@@ -8,11 +7,9 @@ export enum BOT_Type {
     RUN,
 }
 
-
 @ccclass('Bot')
 export class Bot extends Component {
 
-    // from self
     @property({ type: Node, tooltip: "Move Speed" })
     tagetNode: Node = null;
 
@@ -21,6 +18,7 @@ export class Bot extends Component {
 
     private _moveSpeed: number = 100;
     private canAttack: boolean = true;
+    private currentTween: Tween<Node> = null;
 
     protected onLoad(): void {
         this._BOT = BOT_Type.MOVE;
@@ -28,7 +26,6 @@ export class Bot extends Component {
 
     start() {
         let collider = this.getComponent(Collider2D);
-        // Listening to 'onCollisionStay' Events
         collider.on(Contact2DType.BEGIN_CONTACT, this.onCollision, this);
     }
 
@@ -38,24 +35,20 @@ export class Bot extends Component {
         }
     }
 
-
     update(dt: number) {
         if (this.tagetNode && this._BOT !== BOT_Type.STOP) {
-            this.move(dt)
+            this.move(dt);
         }
     }
-
 
     move(dt) {
         let currentPos = this.node.position.clone();
         let targetPos = this.tagetNode.position.clone();
 
-        //xoay theo
         let dir = new Vec3();
         Vec3.subtract(dir, targetPos, currentPos);
         this.node.angle = misc.radiansToDegrees(Math.atan2(dir.y, dir.x));
 
-        // di chuyen
         let distance = dir.length();
         dir.normalize();
 
@@ -63,12 +56,35 @@ export class Bot extends Component {
 
         if (distance < this.rangeAttack()) {
             this.attack();
-            this._BOT = BOT_Type.STOP
+            this._BOT = BOT_Type.STOP;
+            this.scheduleOnce(() => {
+                this.run();
+                console.log(">>>>>>>>>")
+            }, 0.3);
         } else {
             let moveDelta = Vec3.multiplyScalar(new Vec3(), dir, moveStep);
             let newPosition = Vec3.add(new Vec3(), currentPos, moveDelta);
             this.node.position = newPosition;
         }
+    }
+
+    run() {
+        let x = this.randomPos(-1200, 1200);
+        let y = this.randomPos(-700, 700);
+        let targetPos = new Vec3(x, y);
+
+        if (this.currentTween) {
+            this.currentTween.stop();
+        }
+
+        let speed = this._moveSpeed
+        this.currentTween = tween(this.node)
+            .to(2, { position: targetPos })
+            .call(() => {
+                this._BOT = BOT_Type.MOVE;
+                this.currentTween = null;
+            })
+            .start();
     }
 
     attack() {
@@ -95,7 +111,7 @@ export class Bot extends Component {
             sprite.fillRange = 1;
 
             tween(sprite)
-                .to(1, { fillRange: 0 })
+                .to(3, { fillRange: 0 })
                 .call(() => {
                     const uiOpacity = wepon?.getComponent(UIOpacity);
                     if (uiOpacity) {
@@ -108,11 +124,10 @@ export class Bot extends Component {
     }
 
     rangeAttack() {
-        let rangeBody = this.node.getComponent(CircleCollider2D).radius;
-        let rangeWepon = this.node.getChildByPath('Wepon/Image').getComponent(BoxCollider2D).size.width;
-        let rangeHit = rangeBody + rangeWepon;
+        const rangeBody = this.node.getComponent(CircleCollider2D).radius;
+        const rangeWepon = this.node.getChildByPath('Wepon/Image').getComponent(BoxCollider2D).size.width;
+        const rangeHit = rangeBody + rangeWepon;
 
-        // trả ra phạm vi có thể chém -10%
         return rangeHit - rangeHit * 0.2;
     }
 
@@ -120,4 +135,3 @@ export class Bot extends Component {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 }
-
